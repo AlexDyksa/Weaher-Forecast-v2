04ad99e8946f1d07afccc549fc6366e7802afac3
Alex Dyksa <odyksa@myagent.com.ua>
2019-04-11 14:19:24 +0300

    added Routes to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Form from './containers/formContainer';
+import Weather from './containers/weatherContainer';
+import Forecast from './containers/forecastContainer';
 
 class App extends Component {
     render() {
         return (
             <div className="app">
-                <Form />
+                <Route path="/" component={Form} exact />
+
+                <Switch>
+                    <Route path="/" component={Weather} exact />
+                    <Route path="/details" component={Forecast} />
+                </Switch>
             </div>
         );
     }
